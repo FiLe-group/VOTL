2223ea1514750439603ebfbb212ddc68e1d73b60
Kristof <mrmaderat1@gmail.com>
2022-11-17 20:35:11 +0200

    Time formatter (getting duration)

--- src/main/java/votl/utils/message/MessageUtil.java
+++ src/main/java/votl/utils/message/MessageUtil.java
@@ -2,11 +2,19 @@ package votl.utils.message;
 
 import java.awt.Color;
 import java.text.DecimalFormat;
+import java.time.Duration;
 import java.time.temporal.TemporalAccessor;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Optional;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.annotation.Nullable;
 
 import votl.App;
+import votl.utils.exception.FormatterException;
 
 import net.dv8tion.jda.api.interactions.DiscordLocale;
 import net.dv8tion.jda.api.utils.TimeFormat;
@@ -15,8 +23,11 @@ public class MessageUtil {
 
 	private final Random random;
 	private final LocaleUtil lu;
+
+	private final Pattern timePatternFull = Pattern.compile("^(([0-9]+)([smhdw]{1}))+$", Pattern.CASE_INSENSITIVE);
+	private final Pattern timePattern = Pattern.compile("([0-9]+)([smhdw]{1})", Pattern.CASE_INSENSITIVE);
 	
-	private final DecimalFormat decimalFormat = new DecimalFormat("#,###,###");
+	private final DecimalFormat decimalFormat = new DecimalFormat("# ### ###");
 
 	public MessageUtil(App bot) {
 		this.random = bot.getRandom();
@@ -32,6 +43,81 @@ public class MessageUtil {
 			);
 		}
 		return "";
+	}
+
+	private enum TimeFormats{
+		SECONDS('s', 1),
+		MINUTES('m', 60),
+		HOURS  ('h', 3600),
+		DAYS   ('d', 86400),
+		WEEKS  ('w', 604800);
+
+		private final Character character;
+		private final Integer multip;
+
+		private static final HashMap<Character, TimeFormats> BY_CHAR = new HashMap<Character, TimeFormats>();
+
+		static {
+			for (TimeFormats format : TimeFormats.values()) {
+				BY_CHAR.put(format.getChar(), format);
+			}
+		}
+
+		TimeFormats(Character character, Integer multip) {
+			this.character = character;
+			this.multip = multip;
+		}
+
+		public Character getChar() {
+			return character;
+		}
+
+		public Integer getMultip() {
+			return multip;
+		}
+
+		@Nullable
+		public static Integer getMultipByChar(Character c) {
+			return Optional.ofNullable(BY_CHAR.get(c)).map(tf -> tf.getMultip()).orElse(null);
+		}
+	}
+
+	/*
+	 * Duration and Period class have parse() method,
+	 * but they are quite inconvinient, as we want to
+	 * use both duration(h m s) and period(w d).
+	 */
+	public Duration getDuration(String text, boolean allowSeconds) throws FormatterException {
+		if (text == null || text.isEmpty()) {
+			return Duration.ZERO;
+		}
+
+		if (!timePatternFull.matcher(text).matches()) {
+			throw new FormatterException("errors.formatter.no_time_provided");
+		}
+		
+		Matcher timeMatcher = timePattern.matcher(text);
+		Long time = 0L;
+		while (timeMatcher.find()) {
+			Character c = timeMatcher.group(2).charAt(0);
+			if (c.equals('s') && !allowSeconds) {
+				throw new FormatterException("errors.formatter.except_seconds");
+			}
+			Integer multip = TimeFormats.getMultipByChar(c);
+			if (multip == null) {
+				throw new FormatterException("errors.formatter.no_multip");
+			}
+
+			try {
+				time = Math.addExact(time, Math.multiplyExact(Long.valueOf(timeMatcher.group(1)), multip));
+			} catch (NumberFormatException ex) {
+				throw new FormatterException("errors.formatter.parse_long");
+			} catch (ArithmeticException ex) {
+				throw new FormatterException("errors.formatter.long_owerflow");
+			}
+		}
+		
+		return Duration.ofSeconds(time);
 	}
 
 	public String capitalize(final String str) {
